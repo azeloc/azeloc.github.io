be54379e4f5ba8df0739f66d0ab8691234f5043d
Fernando Corrêa <fptcorrea@gmail.com>
2017-03-01 15:10:53 -0300

    arruma os titulos

--- build.R
+++ build.R
@@ -2,8 +2,21 @@ rmds <- list.files("content/post", pattern = "\\.Rmd$", full.names = T)
 
 for(rmd in rmds){
   rmarkdown::render(rmd, output_format = rmarkdown::md_document(
-    variant = 'markdown_github', preserve_yaml = F
+    variant = 'markdown', preserve_yaml = F
   ))
+
+  md <- gsub("\\.Rmd","\\.md",rmd) %>%
+    readr::read_lines()
+
+  titulos <- md %>%
+    stringr::str_detect("title\\:") %>%
+    which %>%
+    dplyr::first()
+
+  md[titulos] <- gsub("title\\: ","title\\: '",md[titulos]) %>%
+    paste0("'")
+
+  readr::write_lines(md, gsub("\\.Rmd","\\.md",rmd))
 }
 
 file.remove(rmds)
